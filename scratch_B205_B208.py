15a6c58f0f42c7d51c059e89fe9c0ddf91775422
Matthew Starke <matthew.j.starke@nasa.gov>
2019-07-22 10:15:53 -0400

    rename variables

--- scratch_B205_B208.py
+++ scratch_B205_B208.py
@@ -35,8 +35,8 @@ globe_tallies = []
 geos_tallies = []
 aqua_tallies = []
 terra_tallies = []
-g15_tallies = []
-g16_tallies = []
+sat_1_tallies = []
+sat_2_tallies = []
 
 globe_categories = ["none", "few", "isolated", "scattered", "broken", "overcast", "obscured"]
 geos_categories = ["none", "few", "isolated", "scattered", "broken", "overcast"]
@@ -51,8 +51,8 @@ for _ in tqdm(range(sample_count), desc="Sampling observations"):
     geos_tally = [sum(1 for ob in sample if ob["tcc_geos_cat"] == category) for category in geos_categories]
     aqua_tally = [sum(1 for ob in sample if ob.tcc_aqua_cat == category) for category in geos_categories]
     terra_tally = [sum(1 for ob in sample if ob.tcc_terra_cat == category) for category in geos_categories]
-    g15_tally = [sum(1 for ob in sample if ob.tcc_geo_cat == category and ob.which_geo == sat_1) for category in geos_categories]
-    g16_tally = [sum(1 for ob in sample if ob.tcc_geo_cat == category and ob.which_geo == sat_2) for category in geos_categories]
+    sat_1_tally = [sum(1 for ob in sample if ob.tcc_geo_cat == category and ob.which_geo == sat_1) for category in geos_categories]
+    sat_2_tally = [sum(1 for ob in sample if ob.tcc_geo_cat == category and ob.which_geo == sat_2) for category in geos_categories]
 
     # Press overcast and obscured into one.
     globe_tally[-2] += globe_tally[-1]
@@ -63,31 +63,31 @@ for _ in tqdm(range(sample_count), desc="Sampling observations"):
     geos_tally = np.array(geos_tally)
     aqua_tally = np.array(aqua_tally)
     terra_tally = np.array(terra_tally)
-    g15_tally = np.array(g15_tally)
-    g16_tally = np.array(g16_tally)
+    sat_1_tally = np.array(sat_1_tally)
+    sat_2_tally = np.array(sat_2_tally)
 
     # Divide by sum to get percentages.
     globe_tally = globe_tally / globe_tally.sum()
     geos_tally = geos_tally / geos_tally.sum()
     aqua_tally = aqua_tally / aqua_tally.sum()
     terra_tally = terra_tally / terra_tally.sum()
-    g15_tally = g15_tally / g15_tally.sum()
-    g16_tally = g16_tally / g16_tally.sum()
+    sat_1_tally = sat_1_tally / sat_1_tally.sum()
+    sat_2_tally = sat_2_tally / sat_2_tally.sum()
 
     globe_tallies.append(globe_tally)
     geos_tallies.append(geos_tally)
     aqua_tallies.append(aqua_tally)
     terra_tallies.append(terra_tally)
-    g15_tallies.append(g15_tally)
-    g16_tallies.append(g16_tally)
+    sat_1_tallies.append(sat_1_tally)
+    sat_2_tallies.append(sat_2_tally)
 
 #################################################
 pop_globe_tally = [sum(1 for ob in filtered_obs if ob.tcc == category) for category in globe_categories]
 pop_geos_tally = [sum(1 for ob in filtered_obs if ob["tcc_geos_cat"] == category) for category in geos_categories]
 pop_aqua_tally = [sum(1 for ob in filtered_obs if ob.tcc_aqua_cat == category) for category in geos_categories]
 pop_terra_tally = [sum(1 for ob in filtered_obs if ob.tcc_terra_cat == category) for category in geos_categories]
-pop_g15_tally = [sum(1 for ob in filtered_obs if ob.tcc_geo_cat == category and ob.which_geo == sat_1) for category in geos_categories]
-pop_g16_tally = [sum(1 for ob in filtered_obs if ob.tcc_geo_cat == category and ob.which_geo == sat_2) for category in geos_categories]
+pop_sat_1_tally = [sum(1 for ob in filtered_obs if ob.tcc_geo_cat == category and ob.which_geo == sat_1) for category in geos_categories]
+pop_sat_2_tally = [sum(1 for ob in filtered_obs if ob.tcc_geo_cat == category and ob.which_geo == sat_2) for category in geos_categories]
 
 pop_globe_tally[-2] += pop_globe_tally[-1]
 pop_globe_tally = pop_globe_tally[:-1]
@@ -96,22 +96,22 @@ pop_globe_tally = np.array(pop_globe_tally)
 pop_geos_tally = np.array(pop_geos_tally)
 pop_aqua_tally = np.array(pop_aqua_tally)
 pop_terra_tally = np.array(pop_terra_tally)
-pop_g15_tally = np.array(pop_g15_tally)
-pop_g16_tally = np.array(pop_g16_tally)
+pop_sat_1_tally = np.array(pop_sat_1_tally)
+pop_sat_2_tally = np.array(pop_sat_2_tally)
 
 pop_globe_tally = pop_globe_tally / pop_globe_tally.sum()
 pop_geos_tally = pop_geos_tally / pop_geos_tally.sum()
 pop_aqua_tally = pop_aqua_tally / pop_aqua_tally.sum()
 pop_terra_tally = pop_terra_tally / pop_terra_tally.sum()
-pop_g15_tally = pop_g15_tally / pop_g15_tally.sum()
-pop_g16_tally = pop_g16_tally / pop_g16_tally.sum()
+pop_sat_1_tally = pop_sat_1_tally / pop_sat_1_tally.sum()
+pop_sat_2_tally = pop_sat_2_tally / pop_sat_2_tally.sum()
 
 globe_sem = np.std(globe_tallies, axis=0, ddof=1) / np.sqrt(sample_count)
 geos_sem = np.std(geos_tallies, axis=0, ddof=1) / np.sqrt(sample_count)
 aqua_sem = np.std(aqua_tallies, axis=0, ddof=1) / np.sqrt(sample_count)
 terra_sem = np.std(terra_tallies, axis=0, ddof=1) / np.sqrt(sample_count)
-g15_sem = np.std(g15_tallies, axis=0, ddof=1) / np.sqrt(sample_count)
-g16_sem = np.std(g16_tallies, axis=0, ddof=1) / np.sqrt(sample_count)
+sat_1_sem = np.std(sat_1_tallies, axis=0, ddof=1) / np.sqrt(sample_count)
+sat_2_sem = np.std(sat_2_tallies, axis=0, ddof=1) / np.sqrt(sample_count)
 
 #################################################
 fig = plt.figure(figsize=(8, 7.2))
@@ -123,16 +123,16 @@ artists.append(ax.bar(np.arange(6) - 0.30, pop_globe_tally, color="purple", widt
 artists.append(ax.bar(np.arange(6) - 0.18, pop_geos_tally, color="orange", width=0.12))
 artists.append(ax.bar(np.arange(6) - 0.06, pop_aqua_tally, color="#6666ff", width=0.12))
 artists.append(ax.bar(np.arange(6) + 0.06, pop_terra_tally, color="#55ff00", width=0.12))
-artists.append(ax.bar(np.arange(6) + 0.18, pop_g15_tally, color=std_colors[sat_1], width=0.12))
-artists.append(ax.bar(np.arange(6) + 0.30, pop_g16_tally, color=std_colors[sat_2], width=0.12))
+artists.append(ax.bar(np.arange(6) + 0.18, pop_sat_1_tally, color=std_colors[sat_1], width=0.12))
+artists.append(ax.bar(np.arange(6) + 0.30, pop_sat_2_tally, color=std_colors[sat_2], width=0.12))
 
 #################################################
 ax.errorbar(np.arange(6) - 0.30, pop_globe_tally, globe_sem, fmt="none", capsize=5, ecolor="black")
 ax.errorbar(np.arange(6) - 0.18, pop_geos_tally, geos_sem, fmt="none", capsize=5, ecolor="black")
 ax.errorbar(np.arange(6) - 0.06, pop_aqua_tally, aqua_sem, fmt="none", capsize=5, ecolor="black")
 artists.append(ax.errorbar(np.arange(6) + 0.06, pop_terra_tally, terra_sem, fmt="none", capsize=5, ecolor="black"))
-ax.errorbar(np.arange(6) + 0.18, pop_g15_tally, g15_sem, fmt="none", capsize=5, ecolor="black")
-ax.errorbar(np.arange(6) + 0.30, pop_g16_tally, g16_sem, fmt="none", capsize=5, ecolor="black")
+ax.errorbar(np.arange(6) + 0.18, pop_sat_1_tally, sat_1_sem, fmt="none", capsize=5, ecolor="black")
+ax.errorbar(np.arange(6) + 0.30, pop_sat_2_tally, sat_2_sem, fmt="none", capsize=5, ecolor="black")
 
 #################################################
 for a in np.arange(-0.5, 5.6, 1.0):
