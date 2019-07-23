c46af755b8e53b36a4d6f35d3aed89872843e101
Matthew Starke <matthew.j.starke@nasa.gov>
2019-07-23 15:22:50 -0400

    new figure B600

---
+++ scratch_B600.py
@@ -0,0 +1,34 @@
+from scratch_vars import *
+import matplotlib.patches as mpatches
+
+plt.figure(figsize=(6, 6))
+ax = plt.axes(projection=ccrs.PlateCarree())
+
+dc_lat = 38.996944
+dc_lon = -76.848333
+ax.set_xlim(dc_lon - 1, dc_lon + 1)
+ax.set_ylim(dc_lat - 1, dc_lat + 1)
+
+ax.add_feature(NaturalEarthFeature("physical", "land", "50m", facecolor="#999999", zorder=-1), edgecolor="#444444", linewidth=1)
+ax.add_feature(NaturalEarthFeature("physical", "ocean", "50m", facecolor="#98B6E2", zorder=-1), edgecolor="#444444", linewidth=1)
+
+ax.scatter([dc_lon], [dc_lat], s=100)
+
+for lat in np.arange(38., 41., 0.25) + 0.125:
+    ax.axhline(lat, color="orange")
+
+for lon in np.arange(-80., -73., 0.3125) + (0.3125 / 2):
+    ax.axvline(lon, color="orange")
+
+ax.add_patch(mpatches.Circle((dc_lon, dc_lat), 0.4, alpha=0.3, edgecolor="cyan", facecolor="cyan", lw=0))
+ax.add_patch(mpatches.Circle((dc_lon, dc_lat), 0.4, fill=False, edgecolor="cyan", lw=3))
+
+states_provinces = NaturalEarthFeature(
+        category='cultural',
+        name='admin_1_states_provinces_lines',
+        scale='50m',
+        facecolor='none')
+
+ax.add_feature(states_provinces, edgecolor="#444444", linewidth=1)
+plt.tight_layout()
+plt.show()
