a76bc7fa5af6028da9d83d57b6b711eaad71ce02
Matthew Starke <matthew.j.starke@nasa.gov>
2019-07-25 16:36:48 -0400

    title and name A080 -> S012

---
+++ figure_S012.py
@@ -0,0 +1,39 @@
+from scratch_vars import *
+
+obs = tools.parse_json("/Users/mjstarke/PycharmProjects/GLOBE/globeqa/examples/"
+                       "land_covers__mosquito_habitat_mapper__tree_heights_20170101_20190531.json")
+tools.do_quality_check(obs, tools.prepare_earth_geometry())
+obs = tools.filter_by_flag_sets(obs, all_of=["LW"])
+
+# Create a figure and axis with cartopy projection.
+ax = plotters.make_pc_fig()
+
+artists = [
+    (
+        plotters.plot_ob_scatter([ob for ob in obs if ob["protocol"] == "land_covers"], ax,
+                                 s=40, marker="_", color="black"),
+        plotters.plot_ob_scatter([ob for ob in obs if ob["protocol"] == "land_covers"], ax,
+                                 s=1600, marker=".", color="black", alpha=0.03)
+    ),
+    (
+        plotters.plot_ob_scatter([ob for ob in obs if ob["protocol"] == "tree_heights"], ax,
+                                 s=40, marker="1", color="green"),
+        plotters.plot_ob_scatter([ob for ob in obs if ob["protocol"] == "tree_heights"], ax,
+                                 s=1600, marker=".", color="green", alpha=0.03)
+    ),
+    (
+        plotters.plot_ob_scatter([ob for ob in obs if ob["protocol"] == "mosquito_habitat_mapper"], ax,
+                                 s=40, marker="x", color="red"),
+        plotters.plot_ob_scatter([ob for ob in obs if ob["protocol"] == "mosquito_habitat_mapper"], ax,
+                                 s=1600, marker=".", color="red", alpha=0.03)
+    ),
+]
+
+ax.legend(artists, ["land_covers", "tree_heights", "mosquito_habitat_mapper"], loc="lower left", fontsize=18)
+
+ax.set_title("Jan 2017 - May 2019 global GLOBE\n"
+             "Locations of GLOBE observations over water given 1:50m NaturalEarth geometry",
+             fontdict={"fontsize": 18})
+
+plt.tight_layout()
+plt.savefig("img/S012_Jan2017-May2019_global_GLOBE-nonSC_scattermap_observations_over_water.png")
