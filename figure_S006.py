9d35d0c8f23b5c1c169b36dccad07705cd4c1216
Matthew Starke <matthew.j.starke@nasa.gov>
2019-07-25 15:27:06 -0400

    fix figure size and spacing

--- figure_S006.py
+++ figure_S006.py
@@ -25,7 +25,8 @@ ax = plotters.plot_stacked_bars(
     x=range(len(flags)),
     ys=y_array,
     labels=[k for k in source_counts],
-    colors=[source_color[k] for k in source_counts]
+    colors=[source_color[k] for k in source_counts],
+    figsize=(8, 6)
 )
 
 ax.set_xticks(range(len(flags)))
@@ -45,14 +46,15 @@ ax = plotters.plot_stacked_bars(
     x=range(len(flags)),
     ys=y_array / np.sum(y_array, axis=0),  # Division by sum makes it proportionwise.
     labels=[k for k in source_counts],
-    colors=[source_color[k] for k in source_counts]
+    colors=[source_color[k] for k in source_counts],
+    figsize=(10, 6)
 )
 
 ax.set_xticks(range(len(flags)))
 ax.set_xticklabels(sorted(flags2.keys()))
 ax.set_xlabel("Flag")
 ax.set_ylabel("Count")
-ax.set_xlim(0.3, 18)  # Make space for the legend.
+ax.set_xlim(-0.7, 18)  # Make space for the legend.
 
 ax.set_title("Jan 2017 - May 2019 global GLOBE\n"
              "Proprotional frequency of quality control flags")
