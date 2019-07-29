f64d0f21eaa545280e2cf64fbea9a91d2057c219
mjstarke <51925016+mjstarke@users.noreply.github.com>
2019-07-29 10:35:14 -0500

    add comments

--- figure_S003.py
+++ figure_S003.py
@@ -2,6 +2,7 @@ from scratch_vars import *
 
 obs = tools.parse_json(fpSC)
 
+# Get number of obs with each cloud cover category.
 vals = tools.find_all_values(obs, "CloudCover")
 keys = ["none", "clear", "isolated", "scattered", "broken", "overcast", "obscured"]
 
@@ -9,9 +10,9 @@ total = sum(vals[k] for k in vals)
 
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111)
-ax.pie([vals[k] for k in keys],
+ax.pie([vals[k] for k in keys],  # Slices ordered by cloud cover category.
        labels=["{} ({:.2%})".format(k, vals[k] / total) for k in keys],
-       labeldistance=None,
+       labeldistance=None,  # Disables automatic labelling outside of legend.
        colors=["#aaaaff", "#8888ff", "#7777dd", "#6666bb", "#555599", "#444477", "#333366"])
 ax.legend()
 ax.set_title("Jan 2017 - May 2019 global GLOBE\nFrequency of each cloud cover category")
