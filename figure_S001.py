00c22f765c27d6a369d1f87b18685f5f0d0370bc
mjstarke <51925016+mjstarke@users.noreply.github.com>
2019-07-29 10:16:57 -0500

    add comments

--- figure_S001.py
+++ figure_S001.py
@@ -2,15 +2,18 @@ from scratch_vars import *
 
 obs = tools.parse_json(fpSC)
 
+# Histogram observations by number of obscurations.
 histo, xedges = np.histogram([len(ob.obscurations) for ob in obs if ob.tcc == "obscured"], bins=np.arange(-0.5, 11.5, 1.))
 
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111)
 ax.bar(np.arange(0, 10.1, 1), np.log10(histo))
 
+# Specify axis ticks.
 minor_ticks = [2, 3, 5, 20, 30, 50, 200, 300, 500, 2000, 3000, 5000, 20000, 30000]
 major_ticks = [1, 10, 100, 1000, 10000]
 
+# Set ticks to log of those values while setting labels to non-log versions.
 ax.set_yticks(np.log10(major_ticks))
 ax.set_yticklabels(major_ticks)
 ax.set_yticks(np.log10(minor_ticks), minor=True)
