1c585a255a84cfd990fb8ac92efe42d51c4a1281
mjstarke <51925016+mjstarke@users.noreply.github.com>
2019-07-30 07:05:26 -0500

    add comments

--- figure_S007.py
+++ figure_S007.py
@@ -2,12 +2,14 @@ from scratch_vars import *
 
 obs = tools.parse_json(fpSC)
 
+# Create a list of dates from 2017 Jan 01 through 2019 May 31.
 day = date(2017, 1, 1)
 dates = []
 while day <= date(2019, 5, 31):
     dates.append(day)
     day += timedelta(days=1)
 
+# Histogram observations by their measured date.
 counts, bin_edges = np.histogram([ob.measured_dt.date() for ob in obs], dates)
 
 fig = plt.figure(figsize=(11, 3.5))
@@ -16,7 +18,7 @@ ax.stackplot(bin_edges[:-1], counts)
 ax.set_xlabel("Date")
 ax.set_ylabel("Daily observations")
 ax.set_xlim(date(2017, 1, 1), date(2019, 5, 31))
-ax.set_ylim(0, 3000)
+ax.set_ylim(0, 3000)  # Cut off eclipse spike.
 ax.grid(axis="y")
 
 ax.text(date(2017, 8, 26), 1080, "North\nAmerican\nEclipse")
