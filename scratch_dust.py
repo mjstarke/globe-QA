c49199396a673302d84983238ed2bab45462fc9f
Matthew Starke <matthew.j.starke@nasa.gov>
2019-08-01 17:58:14 -0400

    new script

---
+++ scratch_dust.py
@@ -0,0 +1,34 @@
+from scratch_vars import *
+
+graph_start = datetime(2017, 1, 1)
+graph_end = datetime(2019, 5, 31)
+
+obs = tools.parse_json(fpSC)
+obs = [ob for ob in obs if "Dust" in ob]
+
+dates = []
+d = graph_start
+while d <= graph_end:
+    dates.append(d)
+    d += timedelta(days=1)
+
+ts = np.histogram([ob.measured_dt for ob in obs], dates)[0]
+
+fig = plt.figure(figsize=(10, 3.5))
+ax = fig.add_subplot(111)
+
+ax.bar(dates[:-1], ts)
+ax.set_xlim(graph_start, graph_end)
+
+ax.title("Jan 2017 - May 2019 global GLOBE"
+         "Number of observations reporting dust per day")
+plt.tight_layout()
+
+
+ax = plotters.make_pc_fig()
+plotters.plot_ob_scatter(obs, ax, c="brown", s=40)
+plotters.plot_ob_scatter(obs, ax, c="brown", s=16000, alpha=0.03)
+
+ax.title("Jan 2017 - May 2019 global GLOBE"
+         "Locations of observations reporting dust")
+plt.tight_layout()
