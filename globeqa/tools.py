39fefa7f27006a8b2718b87df640e1b76061d286
Matthew Starke <matthew.j.starke@nasa.gov>
2019-08-02 17:33:06 -0400

    minimum column width now 1; fixes "'=' alignment" bug

--- globeqa/tools.py
+++ globeqa/tools.py
@@ -328,7 +328,7 @@ def find_all_attributes(obs: List[dict]) -> List[str]:
 
 def pretty_print_dictionary(d: dict, print_percent: bool = True, print_total: bool = True,
                             total: Optional[float] = None, sorting: Union[None, str, Iterable] = "ka",
-                            min_column_widths: Tuple[int, int, int] = (0, 0, 0),
+                            min_column_widths: Tuple[int, int, int] = (1, 1, 1),
                             compress_below: Optional[float] = None) -> list:
     """
     Pretty-prints the contents of a dictionary.
@@ -343,20 +343,26 @@ def pretty_print_dictionary(d: dict, print_percent: bool = True, print_total: bo
     be arbitrary (d.keys() is used).  If "ka", keys will be sorted ascending; "kd" sorts descending.  "va" will sort the
     keys by their corresponding values ascending; "vd" sorts by values descending.  Default "ka".
     :param min_column_widths: A triple of the minimum widths of the three printed columns (key, value, and percentage,
-    respectively).  Default (0, 0, 0).
+    respectively).  Default (1, 1, 1).
     :param compress_below: Items in d whose values are less than compress_below will be compressed into a single
     "(other)" entry at the bottom of the table (unless the sum of these items is 0).  Default None, which compresses no
     items.
     :return: None.  The results are printed in three columns: key, value, percentage (if do_percent).  The columns are
     automatically sized so that two spaces exist between them.  If d contains no keys, nothing is printed.
     :raises TypeError: If sorting is a non-iterable non-string.
-    :raises ValueError: If min_column_widths does not have length 3, or if sorting is an invalid string.
+    :raises ValueError: If min_column_widths does not have length 3.
+    :raises ValueError: If any element of min_column_widths is less than 1.
+    :raises ValueError: If sorting is an invalid string.
     :raises ZeroDivisionError: If total is zero (whether set explicitly or calculated automatically).
     :return: The list of keys in the order that they were printed, or an empty list if no keys were printed.  Note that
     the compression and total rows are not considered keys.
     """
     if len(min_column_widths) != 3:
         raise ValueError("Argument 'min_column_widths' must have length 3.")
+
+    for width in min_column_widths:
+        if width < 1:
+            raise ValueError("All minimum column widths must be at least 1.")
 
     # If there are no keys in the dictionary, there's nothing to do.
     if len(d.keys()) == 0:
