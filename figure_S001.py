1dd2bee5a2558b8bb13b8242ee5381a089e3b2a8
Matthew Starke <matthew.j.starke@nasa.gov>
2019-08-05 15:28:39 -0400

    adjust layout and clarify title

--- figure_S001.py
+++ figure_S001.py
@@ -5,7 +5,7 @@ obs = tools.parse_json(fpSC)
 # Histogram observations by number of obscurations.
 histo, xedges = np.histogram([len(ob.obscurations) for ob in obs if ob.tcc == "obscured"], bins=np.arange(-0.5, 11.5, 1.))
 
-fig = plt.figure(figsize=(6, 6))
+fig = plt.figure(figsize=(7, 6))
 ax = fig.add_subplot(111)
 ax.bar(np.arange(0, 10.1, 1), np.log10(histo))
 
@@ -21,7 +21,8 @@ ax.set_yticklabels(minor_ticks, minor=True)
 ax.grid(which="major", axis="y")
 ax.set_xlabel("Concurrent obscurations")
 ax.set_ylabel("Number of observations")
-ax.set_title("Jan 2017 - May 2019 global GLOBE\nFrequency of concurrent obscurations")
+ax.set_title("Jan 2017 - May 2019 global GLOBE\n"
+             "Number of obscurations for all observations with 'obscured' cloud cover")
 
 plt.tight_layout()
 plt.savefig("img/S001_Jan2017-May2019_global_GLOBE_bar_concurrent_obscurations.png")
