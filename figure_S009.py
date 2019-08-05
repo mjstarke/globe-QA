d0dc082d5b465a1daed2bb2420a581ec8acc2b9a
Matthew Starke <matthew.j.starke@nasa.gov>
2019-08-05 15:24:21 -0400

    adjust layout

--- figure_S009.py
+++ figure_S009.py
@@ -3,7 +3,7 @@ from scratch_vars import *
 obs = tools.parse_json(fpSC)
 obs = [ob for ob in obs if not (-300 <= ob.elevation <= 6000)]
 
-ax = plotters.make_pc_fig()
+ax = plotters.make_pc_fig(figsize=(15.5, 9))
 
 sources = tools.find_all_values(obs, "DataSource")
 artists = []
@@ -14,9 +14,9 @@ for source in sources:
     bubble = plotters.plot_ob_scatter(theseObs, ax, s=1600, marker=".", color=source_color[source], alpha=0.02)
     artists.append((dot, bubble))
 
-ax.legend(artists, sources, loc="lower left", fontsize=18)
+ax.legend(artists, sources, fontsize=18)
 ax.set_title("Jan 2017 - May 2019 global GLOBE\n"
              "Locations of observations whose elevations lie outside the range -300m to 6000m",
              fontdict={"fontsize": 18})
-
+plt.tight_layout()
 plt.savefig("img/S009_Jan2017-May2019_global_GLOBE_scattermap_suspect_elevation.png")
