86b16fddbd63d798fee55a6b227895f62c3bf6bc
Matthew Starke <matthew.j.starke@nasa.gov>
2019-08-06 13:27:33 -0400

    adjust labels

--- figure_S007.py
+++ figure_S007.py
@@ -15,8 +15,8 @@ counts, bin_edges = np.histogram([ob.measured_dt.date() for ob in obs], dates)
 fig = plt.figure(figsize=(11, 3.5))
 ax = fig.add_subplot(111)
 ax.stackplot(bin_edges[:-1], counts)
-ax.set_xlabel("Date")
-ax.set_ylabel("Daily observations")
+ax.set_xlabel("Date (UTC)")
+ax.set_ylabel("Observations per day")
 ax.set_xlim(date(2017, 1, 1), date(2019, 5, 31))
 ax.set_ylim(0, 3000)  # Cut off eclipse spike.
 ax.grid(axis="y")
@@ -24,8 +24,8 @@ ax.grid(axis="y")
 ax.text(date(2017, 8, 26), 1080, "North\nAmerican\nEclipse")
 ax.text(date(2018, 5, 5), 1080, "NASA GLOBE\nClouds Data\nChallenge")
 
-ax.set_title("Jan 2017 - May 2019 global GLOBE\n"
+ax.set_title("Jan 2017 - May 2019 / Global / GLOBE clouds\n"
              "Number of observations per day")
 
 plt.tight_layout()
-plt.savefig("img/S007_Jan2017-May2019_global_GLOBE_timeseries_observations_per_day.png")
+plt.savefig("img/S007_Jan2017-May2019_global_GLOBE-SC_timeseries_observations_per_day.png")
