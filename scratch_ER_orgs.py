4383730398e1e3c086ac6f301dbc375e071ed81e
Matthew Starke <matthew.j.starke@nasa.gov>
2019-08-08 17:51:14 -0400

    add summary docstring

--- scratch_ER_orgs.py
+++ scratch_ER_orgs.py
@@ -1,3 +1,9 @@
+"""
+Finds all observations that have a negative elevation and do not come from GLOBE Observer, then prints two tables: one
+containing the names of organizations that make those observations, and one contianing the sites from which those
+observations are made.
+"""
+
 from figure_common import *
 
 path = tools.download_from_api(["sky_conditions"], date(2017, 1, 1), date(2019, 7, 11))
