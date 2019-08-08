7bab07b86a87dc99f76a4318cf43531fee859e04
Matthew Starke <matthew.j.starke@nasa.gov>
2019-08-08 11:22:27 -0400

    new figure S024

---
+++ figure_S024.py
@@ -0,0 +1,35 @@
+from scratch_vars import *
+
+obs = tools.parse_json(fpSC)
+
+bottom = np.zeros(60)
+artists = []
+sources = ["GLOBE Observer App", "GLOBE Data Entry Web Forms", "GLOBE Data Entry App"]
+
+fig = plt.figure(figsize=(11, 5))
+ax = fig.add_subplot(111)
+
+for source in sources:
+
+    minutes = [ob.measured_dt.minute for ob in obs if ob.source == source]
+
+    histo, bin_lefts = np.histogram(minutes, np.arange(0, 61, 1))
+
+    artists.append(
+        ax.bar(bin_lefts[:-1], histo, align="edge", width=1, color=source_color[source], bottom=bottom)
+    )
+
+    bottom += histo
+
+ax.set_xticks(np.arange(0, 60, 5))
+ax.set_xticklabels([":{:0>2.0f}".format(v) for v in np.arange(0, 60, 5)] + [":00"])
+ax.set_xlim(0, 60)
+ax.set_xlabel("Minute (UTC)")
+ax.set_ylabel("Count")
+
+ax.set_title("Jan 2017 - May 2019 / Global / GLOBE Clouds\n"
+             "Temporal distribution of observations (minute only)")
+
+plt.tight_layout()
+ax.legend(artists, sources)
+plt.savefig("img/S024_Jan2017-May2019_global_GLOBE-SC_histogram_minute.png")
